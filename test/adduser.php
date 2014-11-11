e9574e4829e7d541dd5dfcd841de2a31dda74107
Naing Lin Aung <nainglinaung@aceplussolutions.com>
2014-11-11 14:58:24 +0900

    html formatting @ adduser.php

--- test/adduser.php
+++ test/adduser.php
@@ -11,27 +11,27 @@ $_SESSION['form_token'] = $form_token;
 ?>
 
 <html>
-<head>
-<title>PHPRO Login</title>
-</head>
+	<head>
+		<title>PHPRO Login</title>
+	</head>
 
-<body>
-<h2>Add user</h2>
-<form action="adduser_submit.php" method="post">
-<fieldset>
-<p>
-<label for="phpro_username">Username</label>
-<input type="text" id="phpro_username" name="phpro_username" value="" maxlength="20" />
-</p>
-<p>
-<label for="phpro_password">Password</label>
-<input type="text" id="phpro_password" name="phpro_password" value="" maxlength="20" />
-</p>
-<p>
-<input type="hidden" name="form_token" value="<?php echo $form_token; ?>" />
-<input type="submit" value="&rarr; Login" />
-</p>
-</fieldset>
-</form>
-</body>
+	<body>
+		<h2>Add user</h2>
+		<form action="adduser_submit.php" method="post">
+		<fieldset>
+			<p>
+				<label for="phpro_username">Username</label>
+				<input type="text" id="phpro_username" name="phpro_username" value="" maxlength="20" />
+			</p>
+			<p>
+				<label for="phpro_password">Password</label>
+				<input type="text" id="phpro_password" name="phpro_password" value="" maxlength="20" />
+			</p>
+			<p>
+				<input type="hidden" name="form_token" value="<?php echo $form_token; ?>" />
+				<input type="submit" value="&rarr; Login" />
+			</p>
+		</fieldset>
+		</form>
+	</body>
 </html>
